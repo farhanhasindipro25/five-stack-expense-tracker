a1ff0f69d7b5e305c7bd6f80dbceaf0eb0e3721e
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-08-23 21:50:17 +0600

    final commit

--- scripts/player-panel.js
+++ scripts/player-panel.js
@@ -1,35 +1,37 @@
 const fiveStack = [];
 
-function disableAfterOneSelect(){
-    const selectButton = document.getElementsById('player-select-btn').addEventListener('click',function(){
-        selectButton.setAttribute('disabled',true);
+function disableAfterOneSelect() {
+  const selectButton = document
+    .getElementsById("player-select-btn")
+    .addEventListener("click", function () {
+      selectButton.setAttribute("disabled", true);
     });
 }
 
-function addToPlayerListPanel(element){
-    const playerName = element.parentNode.children[0].innerText;
-    element.setAttribute('disabled',true);
-    fiveStack.push(playerName);
-    if(fiveStack.length === 6){
-        element.removeAttribute('disabled',false);
-        alert('You cannot select more than 5 players');
-        return;
-    }
-    getPlayerQuantity('player-quantity');
-    displayPlayers(fiveStack);
+function addToPlayerListPanel(element) {
+  const playerName = element.parentNode.children[0].innerText;
+  element.setAttribute("disabled", true);
+  fiveStack.push(playerName);
+  if (fiveStack.length === 6) {
+    element.removeAttribute("disabled", false);
+    alert("You cannot select more than 5 players");
+    return;
+  }
+  getPlayerQuantity("player-quantity");
+  displayPlayers(fiveStack);
 }
 
-function getPlayerQuantity(elementId){
-    document.getElementById(elementId).innerText = fiveStack.length; 
+function getPlayerQuantity(elementId) {
+  document.getElementById(elementId).innerText = fiveStack.length;
 }
 
-function displayPlayers(fiveStack){
-    const playerListPanel = document.getElementById('player-list-panel');
-    playerListPanel.innerText = '';
-    for(let i = 0 ; i < fiveStack.length ; i++){
-        const name = fiveStack[i];
-        const displayPlayer = document.createElement('li');
-        displayPlayer.innerText = name;
-        playerListPanel.appendChild(displayPlayer);
-    }
+function displayPlayers(fiveStack) {
+  const playerListPanel = document.getElementById("player-list-panel");
+  playerListPanel.innerText = "";
+  for (let i = 0; i < fiveStack.length; i++) {
+    const name = fiveStack[i];
+    const displayPlayer = document.createElement("li");
+    displayPlayer.innerText = name;
+    playerListPanel.appendChild(displayPlayer);
+  }
 }
